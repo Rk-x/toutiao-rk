25f2a59d8def271eca5f108c30bebb21a84175dd
luolin <1624603357@qq.com>
2023-06-19 22:09:06 +0800

    项目初始化-调整初始目录结构

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+//  加载全局样式
+import './style/index.less'
 Vue.config.productionTip = false
-
 new Vue({
   router,
   store,
