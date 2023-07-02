2322cd2d7acbc4f3fbab7996c818fece8360801c
luolin <1624603357@qq.com>
2023-07-02 23:20:09 +0800

    完成登陆后的TabBar以及路由跳转，我的页面未登录状态展示

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,47 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+
 Vue.use(VueRouter)
-// 配置路由表
+
+// 路由表
 const routes = [
   {
     path: '/login',
     name: 'login',
-    component: () => import('@/views/login/index.vue')
+    component: () => import('@/views/login')
+  },
+  {
+    path: '/',
+    // name: 'layout', // 如果父路由有默认子路由，那它的 name 没有意义
+    component: () => import('@/views/layout'),
+    redirect: '/home',
+    children: [
+      {
+        path: '/home', // 默认子路由，只能有1个
+        name: 'home',
+        component: () => import('@/views/home')
+      },
+      {
+        path: '/qa',
+        name: 'qa',
+        component: () => import('@/views/qa')
+      },
+      {
+        path: '/video',
+        name: 'video',
+        component: () => import('@/views/video')
+      },
+      {
+        path: '/my',
+        name: 'my',
+        component: () => import('@/views/my')
+      }
+    ]
   }
 ]
+
 const router = new VueRouter({
   routes
 })
+
 export default router
---
+++ src/views/layout/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="layout-container">
+    <!--子路由的出口-->
+    <router-view></router-view>
+
+    <van-tabbar class="layout-tabbar"  route>
+      <van-tabbar-item to="/home" >
+        <i slot="icon" class="toutiao rk-shouye"></i>
+        <span class="text">首页</span>
+      </van-tabbar-item>
+      <van-tabbar-item to="/qa">
+        <i slot="icon" class="toutiao rk-wenda"></i>
+        <span class="text">问答</span>
+      </van-tabbar-item>
+      <van-tabbar-item to="/video">
+        <i slot="icon" class="toutiao rk-shipin"></i>
+        <span class="text">视频</span>
+      </van-tabbar-item>
+      <van-tabbar-item to="/my">
+        <i slot="icon" class="toutiao rk-wode"></i>
+        <span class="text">我的</span>
+      </van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.layout-container{
+    .layout-tabbar{
+        i.toutiao{
+            font-size: 40px;
+        }
+    }
+}
+</style>
